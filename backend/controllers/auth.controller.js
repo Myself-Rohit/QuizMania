8b52b3cb3b5413948a7a5e8e908c268260b8a6bd
rohit <rohitnegi263139@gmail.com>
2025-05-02 10:30:05 +0530

    initial setup and connect database

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,6 @@
+export const signup = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(400).json({ message: error?.message });
+  }
+};
---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import connectDB from "./db/connect.js";
+import authRoute from "./routes/auth.route.js";
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+dotenv.config();
+const port = process.env.PORT || 3001;
+connectDB(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Database Connected!!");
+    app.listen(port, () => {
+      console.log(`app running on port: ${port}`);
+    });
+  })
+  .catch((err) => console.log(err));
+
+app.use("/auth", authRoute);
